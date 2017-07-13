9ab9ccb6b5417e9a108df8a9df77ed6004992f84
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2017-07-13 17:32:51 -0300

    Inserção do módulo Forms no módulo do Login

--- fan-app/src/app/home/login/login.module.ts
+++ fan-app/src/app/home/login/login.module.ts
@@ -1,11 +1,12 @@
 import { LoginComponent } from './login.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule
     ],
     declarations: [LoginComponent],
     exports: [LoginComponent]
