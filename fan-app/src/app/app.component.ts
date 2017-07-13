59c8a40cea645701c07e3a06c84568d927c3b8ac
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2017-07-13 15:44:46 -0300

    Login da aplicação e aquisicao do token quando a pagina é iniciada

--- fan-app/src/app/app.component.ts
+++ fan-app/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
+import { WebService } from './model-controller-angular2/model/webservice/WebService';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ WebService ]
 })
 export class AppComponent {
   title = 'app works!';
 
-  /*AppComponent (public navCtrl: ) {
-
+  constructor(private webService: WebService) {
+    /** Busca o token de seguranca da aplicacao.*/
+    /**Realiza o login da aplicacao afim de pegar o token de
+    e guardar no storage do angular, para poder realizar as operecoes
+    que sao somente de aplicacao.*/
+    webService.loginAplicacao();
   }
-
-  Login() {
-    this.nav
-  }*/
 }
