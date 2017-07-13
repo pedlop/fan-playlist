24d0d1e7bf9c164092b7178227242d97472f78a4
joao <joao_camargo10@hotmail.com>
2017-07-13 15:36:03 -0300

    #Commit comunicação login

--- fan-app/src/app/home/login/login.component.ts
+++ fan-app/src/app/home/login/login.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Controller } from '../../model-controller-angular2/controller/Controller';
+import { WebService } from '../../model-controller-angular2/model/webservice/WebService';
+import { ExcecaoNegocio } from '../../model-controller-angular2/model/excecao/ExcecaoNegocio';
+import { Dto } from '../../model-controller-angular2/model/dto/Dto';
+import { UsuarioDTO } from '../../model-controller-angular2/model/dto/UsuarioDTO';
+import { DnsWebService } from '../../model-controller-angular2/model/webservice/DnsWebService';
 
 @Component({
   selector: 'app-login',
+  providers: [WebService],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public controller: ContrLoginRedePontal;
+  /* Dados de login.*/
+  public usuarioDTO: UsuarioDTO = new UsuarioDTO();
+
+  constructor(private webService: WebService, public router: Router) {
+    this.controller = new ContrLoginRedePontal();
+    this.controller.setFormCdsClienteComponent(this, this.webService);
+  }
 
   ngOnInit() {
   }
 
+  public logar(): void {
+    /* Preenche os DTO(s).*/
+    const prmtr: ParametroLoginRedePontal = new ParametroLoginRedePontal();
+
+    if (this.controller.tokenSincronizacaoOK) {
+      this.webService.pegarTokenSincronizacao(true).subscribe((rest) => {
+        this.controller.setFormCdsClienteComponent(this, this.webService);
+        this.webService.loginUsuario(this.usuarioDTO, this.controller);
+        this.controller.tokenSincronizacaoOK = false;
+      },
+        (erro: any) => {
+          this.controller.tokenSincronizacaoOK = true;
+        });
+    } else {
+      //requisicao ja enviada
+    }
+  }
+
+}
+
+class ContrLoginRedePontal extends Controller {
+  public tokenSincronizacaoOK = true;
+  private component: LoginComponent;
+  private webservice : WebService;
+
+  setFormCdsClienteComponent(f: LoginComponent,webservice : WebService) {
+    this.component = f;
+    this.webservice = webservice;
+  }
+
+  casoErroRede(): void {
+    this.tokenSincronizacaoOK = true;
+  }
+
+  casoErroNegocio(): void {
+    const erro: ExcecaoNegocio = JSON.parse(this.response.text());
+    this.tokenSincronizacaoOK = true;
+    console.log('ERRO DE NEGOCIO. ' + erro.codigoErro + '/' + erro.nomeClasse + '/' + erro.controller);
+  }
+
+  casoComunicacaoCompleta(): void {
+    // this.component.alerts.push({
+    //   type: 'success',
+    //   msg: 'Login realizado com sucesso!',
+    //   timeout: 2000
+    // });
+    this.tokenSincronizacaoOK = true;
+    /*Guarda o token de seguranca do usuario na memoria./
+    localStorage.setItem(DnsWebService.storageTokenUsuario,
+    this.response.text());
+  }
+}
+
+class ParametroLoginRedePontal extends Dto {
+  public loginDTO: UsuarioDTO;
 }
